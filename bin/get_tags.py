1e7aea71afad1278b234feebb56da4f3087f19e5
Doug Emery <emeryr@upenn.edu>
2014-10-20 21:35:06 -0400

    Extract text and raw tags.

--- bin/get_tags.py
+++ bin/get_tags.py
@@ -1,49 +1,46 @@
 import os
 import json
-# files = [ 'POP_0001.json.fixed' ]
 
-files = [ 'POP_0001.json.fixed',
-          'POP_0002.json.fixed',
-          'POP_0003.json.fixed',
-          'POP_0004.json.fixed',
-          'POP_0005.json.fixed',
-          'POP_0006.json.fixed',
-          'POP_0007.json.fixed',
-          'POP_0008.json.fixed',
-          'POP_0009.json.fixed',
-          'POP_0010.json.fixed',
-          'POP_0011.json.fixed',
-          'POP_0012.json.fixed',
-          'POP_0013.json.fixed',
-          'POP_0014.json.fixed',
-          'POP_0015.json.fixed',
-          'POP_0016.json.fixed',
-          'POP_0017.json.fixed',
-          'POP_0018.json.fixed',
-          'POP_0019.json.fixed',
-          'POP_0020.json.fixed',
-          'POP_0021.json.fixed',
-          'POP_0022.json.fixed',
-          'POP_0023.json.fixed',
-          'POP_0024.json.fixed' ]
 
+files = [ 'POP_0001.json',
+          'POP_0002.json',
+          'POP_0003.json',
+          'POP_0004.json',
+          'POP_0005.json',
+          'POP_0006.json',
+          'POP_0007.json',
+          'POP_0008.json',
+          'POP_0009.json',
+          'POP_0010.json',
+          'POP_0011.json',
+          'POP_0012.json',
+          'POP_0013.json',
+          'POP_0014.json',
+          'POP_0015.json',
+          'POP_0016.json',
+          'POP_0017.json',
+          'POP_0018.json',
+          'POP_0019.json',
+          'POP_0020.json',
+          'POP_0021.json',
+          'POP_0022.json',
+          'POP_0023.json',
+          'POP_0024.json' ]
 
 tags = dict();
+this_dir = os.path.dirname(__file__)
 
-for file in files: 
-    blerg = json.load(open(file))
+for file in files:
+    blerg = json.load(open(os.path.join(this_dir, '../data', file)))
 
     for item in blerg:
         for tag in item['photo']['tags']['tag']:
-            raw = tag['raw']
-            if (not raw in tags):
-                tags[raw] = tag
-            # try: 
-            #     set.add(tag['raw'])
-            # except: 
-            #     print "Troubles with %s" % tag['id']
+            text = tag['text']
+            if (not text in tags):
+                tags[text] = tag
 
-for raw in tags:
-    t = str(raw.encode("utf-8"))
-    a = str(tags[raw]['authorname'].encode("utf-8"))
-    print "\"%s\",\"%s\"" % (t, a)
+for text in tags:
+    t = str(text.encode('utf-8'))
+    r = str(tags[text]['raw'].encode('utf-8'))
+    a = str(tags[text]['authorname'].encode('utf-8'))
+    print "\"%s\",\"%s\",\"%s\"" % (t, r, a)
