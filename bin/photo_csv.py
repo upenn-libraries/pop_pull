6c7dc8d60ee422f560c7e708af1d339f6edbcb0a
Doug Emery <emeryr@upenn.edu>
2015-07-28 14:53:15 -0400

    CSV creation: UTF-8; Book model

--- bin/photo_csv.py
+++ bin/photo_csv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
@@ -5,11 +6,80 @@ import sys
 import json
 import re
 import csv
+import codecs
+import unicodedata
 
 json_re = re.compile('\.json$')
 files = []
+# files.append(so.path.join(os.path.dirname(__file__), '..', 'POP_0001.json'))
 data_dir =  os.path.join(os.path.dirname(__file__), '..', 'data')
 rows = []
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf-8')(sys.stderr)
+
+class Book:
+    NORMAL_RE  = re.compile('\W')
+    FOLIO_RE   = re.compile('^Folio\s*', re.IGNORECASE)
+
+    names = ('call',
+             'bibid',
+             'title',
+             'author',
+             'conference',
+             'pub',
+             'date',
+             'provenance',
+             'printplace')
+
+    def __init__(self, line):
+        self._line = line.rstrip('\r\n')
+        self._parts = self._line.split('\t')
+        self._props = {}
+        # call, bibid, title, author, conference, pub, date, provenance, printplace
+        for i in xrange(len(self.names)):
+            if len(self._parts) > i:
+                self._props[self.names[i]] = self._parts[i]
+
+    @property
+    def normal_callno(self):
+        s = self._props['call']
+        if self.FOLIO_RE.search(s):
+            s = self.FOLIO_RE.sub('', s)
+            s += 'folio'
+        return self.NORMAL_RE.sub('', s.lower())
+
+class Books:
+
+    def __init__(self, tsv):
+        self._tsv = tsv
+        self._call_to_bibid = {}
+        self._books = []
+        self._callno_map = {}
+        self._index = 0
+        self._load_tsv()
+
+    def _load_tsv(self):
+        for line in codecs.open(self._tsv, 'r', 'utf-8'):
+            try:
+                book = Book(line)
+                self._books.append(book)
+                self._callno_map[book.normal_callno] = book
+            except ValueError as ex:
+                print "Bad line: %s" % (line,)
+                raise ex
+
+    def __iter__(self):
+        return self
+
+    def find(self, normal_callno):
+        return self._callno_map.get(normal_callno, None)
+
+    def next(self):
+        if self._index >= len(self._books):
+            raise StopIteration
+        self._index += 1
+        return self._books[self._index - 1]
 
 class Categories:
     NOT_FOUND = ('NOT_FOUND', 'NOT_FOUND')
@@ -23,18 +93,17 @@ class Categories:
         self.load_csv()
 
     def load_csv(self):
-        rdr = csv.reader(open(self.csv))
-        for row in rdr:
+        for line in codecs.open(self.csv, 'r', 'utf-8'):
             try:
                 # Ugly	Clean	Mitch's Category	Laura's Category	Model	Field	Basis
                 # zaravoni	Zaravoni	Person	Person	Copy	Author	Transcription | Secondary | Imagery Description | Heraldry Description
-                ugly, raw, mitch, laura, model, field, basis = row[:]
+                ugly, raw, mitch, laura, model, field = line.rstrip('\n').split('\t')
                 # ugly, raw, model, field = row[:]
                 cat = (model.strip().lower(),field.strip().lower())
-                self.tags[ugly] = cat
+                self.tags[raw] = cat
                 self.cat_set.add(cat)
             except:
-                print "Problem: %r" % row
+                print "Problem: %r" % line
                 raise
         self.cat_set.add(Categories.NOT_FOUND)
         self.cats = list(self.cat_set)
@@ -44,6 +113,7 @@ class Photo:
     def __init__(self,record):
         "Create a photo thingy."
         self.photo_dict = record
+        self._tag_map = {}
 
     @property
     def photo(self):
@@ -54,22 +124,26 @@ class Photo:
 
     @property
     def pid(self):
-        return self._stringify(self.photo['id'])
+        # return self._stringify(self.photo['id'])
+        return self.photo['id']
 
     @property
     def title(self):
-        return self._stringify(self.photo['title'])
+        # print 'title: %s' % (self.photo['title'], )
+        # return self._stringify(self.photo['title'])
+        return self.photo['title']
 
     @property
     def url(self):
-        return self._stringify(self.photo['urls']['url'][0]['text'])
+        # return self._stringify(self.photo['urls']['url'][0]['text'])
+        return self.photo['urls']['url'][0]['text']
 
     @property
     def tags(self):
         return self.photo['tags']['tag']
 
     def all_tag_vals(self, key):
-        return [ self._stringify(x[key]) for x in self.tags]
+        return [ x[key] for x in self.tags]
 
     @property
     def raw_tags(self):
@@ -79,46 +153,70 @@ class Photo:
     def text_tags(self):
         return self.all_tag_vals('text')
 
-    def mapped_tags(self, categories):
-        tag_map = {}
-        tag_map[Categories.NOT_FOUND] = []
+    def map_tags(self, categories):
+        self._tag_map[Categories.NOT_FOUND] = []
         for cat in categories.cats:
-            tag_map[cat] = []
+            self._tag_map[cat] = []
 
-        for tag in self.text_tags:
-            cat = categories.tags[tag] if tag in categories.tags else Categories.NOT_FOUND
-            tag_map[cat].append(tag)
-        return tag_map
+        for tag in self.raw_tags:
+            tag = unicodedata.normalize('NFC', tag)
+            if tag in categories.tags:
+                cat = categories.tags[tag]
+            else:
+                cat = Categories.NOT_FOUND
+                sys.stderr.write("Tag not found: %s; photo_id: %s\n" % (tag, self.pid))
+            self._tag_map[cat].append(tag)
 
-def stringify(s):
-    return s.encode('utf-8')
+    def mapped_tags(self, categories):
+        if len(self._tag_map) == 0:
+            self.map_tags(categories)
+        return self._tag_map
+
+    def call_nos(self):
+        if len(self._tag_map) > 0 and self._tag_map.get((u'copy', u'call number'), None):
+            return self._tag_map[(u'copy',u'call number')]
+        else:
+            return []
 
 cats = Categories(sys.argv[1])
 print cats.cats
 
-for fname in os.listdir(data_dir):
+books = Books(os.path.join(data_dir, 'call_bidid_map.tsv'))
+
+# get the Photo JSON files
+for fname in os.listdir(os.path.join(data_dir, 'NFC')):
     if re.search(json_re, fname):
         files.append(fname)
 
 photos = None
 
-head = [ 'ID', 'Title', 'URL' ]
+
+head = [ 'ID', 'Title', 'URL', 'BibID' ]
 for cat in cats.cats:
     head.append(':'.join(cat))
 
 for fname in files:
-    records = json.load(open(os.path.join(data_dir,fname)))
+    records = json.load(codecs.open(os.path.join(data_dir,'NFC',fname), 'r', 'utf-8'))
     for record in records:
         photo = Photo(record)
         row = [ photo.pid, photo.title, photo.url ]
         tags = photo.mapped_tags(cats)
+        bibids = []
+        for call in photo.call_nos():
+            book = books.find(call)
+            if book:
+                bibids.append(book._props['bibid'])
+        row.append('|'.join(bibids))
         for cat in cats.cats:
             row.append('|'.join(tags[cat]))
         rows.append(row)
 
-outfile = open("photos_mapped.csv", "w+")
-csv_writer = csv.writer(outfile)
-csv_writer.writerow(head)
-csv_writer.writerows(rows)
+outfile = codecs.open("photos_mapped.csv", "w+", 'utf-8')
+outfile.write(u'\t'.join(head))
+outfile.write('\n')
+for row in rows:
+    outfile.write(u'\t'.join(row))
+    outfile.write('\n')
+
 outfile.close()
 print "Wrote \"%s\"" % outfile.name
