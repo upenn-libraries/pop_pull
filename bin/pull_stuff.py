6852f45280177b7f96d6868554ba1cc23dd5c4dc
Doug Emery <emeryr@upenn.edu>
2015-07-28 14:50:44 -0400

    Pull script handles Unicode weirdness

--- bin/pull_stuff.py
+++ bin/pull_stuff.py
@@ -2,6 +2,8 @@ import json
 import os
 import sys
 import time
+import codecs
+import re
 
 from exceptions import Exception
 
@@ -9,6 +11,12 @@ import flickr_api
 from flickr_api.api import flickr
 
 error_file = 'errors.txt'
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+rtl = '\u200F'.decode( 'unicode-escape' )
+ltr = '\u200E'.decode( 'unicode-escape' )
+bidi_re = re.compile("[%s%s]+" % (rtl, ltr), re.UNICODE)
+
 
 # flickr.photos.getInfo
 #
@@ -35,16 +43,18 @@ def write_info(photos, key):
     print "Writing %03d photos; page %04d of %04d %22s" % (stats.perpage, stats.page, stats.pages, tstamp)
 
     fname = "POP_%04d.json" % stats.page
-    f = open(fname, 'w+')
+    f = codecs.open(fname, 'w+', 'utf-8')
     f.write('[')
     for photo in photos:
         try:
-            f.write(json.dumps(get_info(photo,key)))
+            s = json.dumps(get_info(photo,key)).decode('utf-8')
+            s = bidi_re.sub('', s)
+            f.write(s)
             f.write(',')
         except Exception as ex:
             print "Caught an exception: %s" % str(ex)
             err = open(error_file, 'a+')
-            err.write("%s - %s" % (str(photo.id), str(ex)))
+            err.write("ERROR: %s - %s - %s\n" % (time.strftime('%Y-%m-%d %H:%M:%S'), str(photo.id).strip(), str(ex)))
             err.close()
 
     f.seek(-1, os.SEEK_END)
@@ -56,8 +66,8 @@ def write_info(photos, key):
 key        = os.environ['POP_FLICKR_KEY']
 secret     = os.environ['POP_FLICKR_SECRET']
 page_size  = 500
-start_page = 6
-end_page   = 6
+start_page = 3
+end_page   = 24
 
 flickr_api.set_keys(api_key = key, api_secret = secret)
 
@@ -67,6 +77,8 @@ user = flickr_api.Person.findByEmail('pennrare@gmail.com')
 photos = user.getPublicPhotos(per_page=page_size, page=start_page)
 pages = photos.info.pages
 write_info(photos, key)
-# for page in xrange(start_page,end_page):
-#     photos = user.getPublicPhotos(per_page=page_size,page=(page+1))
-#     write_info(photos,key)
+for page in xrange(start_page,end_page):
+    print "Sleeping for 10 minutes before next download..."
+    time.sleep(600)
+    photos = user.getPublicPhotos(per_page=page_size,page=(page+1))
+    write_info(photos,key)
